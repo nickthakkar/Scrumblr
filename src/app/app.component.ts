f3b4f0db3b05778dd4ddbc76bf9b59cb057faad0
Nick Thakkar <Nick@Nicks-mac.local>
2018-02-28 22:54:36 -0500

    ionic app setup

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -21,8 +21,8 @@ export class MyApp {
 
     // used for an example of ngFor and navigation
     this.pages = [
-      { title: 'Home', component: HomePage },
-      { title: 'List', component: ListPage }
+      { title: 'Fibonaci', component: HomePage },
+      { title: 'Scrum', component: ListPage }
     ];
 
   }
