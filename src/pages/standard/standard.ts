e23f1ca78503ca785c728d0fad33794030d9d682
Nick Thakkar <Nick@Nicks-mac.local>
2018-10-03 20:02:28 -0400

    add some style and structural changes

--- src/pages/standard/standard.ts
+++ src/pages/standard/standard.ts
@@ -2,10 +2,10 @@ import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
 
 @Component({
-  selector: 'page-home',
-  templateUrl: 'home.html'
+  selector: 'page-standard',
+  templateUrl: 'standard.html'
 })
-export class HomePage {
+export class StandardPage {
 
   constructor(public navCtrl: NavController) {
 
